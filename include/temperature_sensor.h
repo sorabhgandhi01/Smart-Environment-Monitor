84ecbb2ed48d907bb61db7267962d549ddd16b01
Sorabh Gandhi <sorabh.gandhi@colorado.edu>
2019-03-28 10:50:23 -0700

    Adding code to access the resgisters of temperature sensor via I2C bus

---
+++ include/temperature_sensor.h
@@ -0,0 +1,27 @@
+
+#ifndef TEMPERATURE_SENSOR
+#define TEMPERATURE_SENSOR
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "i2c_helper.h"
+
+#define TEMPERATURE_SENSOR_ADDR (0x48)
+
+#define TEMPERATURE_REG         (0x00)
+#define CONFIGURATION_REG       (0X01)
+#define TLOW_REG                (0X02)
+#define THIGH_REG               (0x03)
+
+int write_tlow_reg(uint16_t temp);
+
+int read_tlow_reg(uint16_t *temp);
+
+int write_thigh_reg(uint16_t temp);
+
+int read_thigh_reg(uint16_t *temp);
+
+int get_sensortemp(uint16_t *temp);
+
+#endif
---
+++ src/temperature_sensor.c
@@ -0,0 +1,64 @@
+
+#include "temperature_sensor.h"
+
+int write_tlow_reg(uint16_t temp)
+{
+
+    if ((temp < -55) || (temp > 150))
+        temp = 75;
+
+    int status = i2c_write_word(TEMPERATURE_SENSOR_ADDR, temp, TLOW_REG);
+
+    return status;
+}
+
+int write_thigh_reg(uint16_t temp)
+{   
+    if ((temp <= -55) || (temp >= 150))
+        temp = 80;
+
+    int status = i2c_write_word(TEMPERATURE_SENSOR_ADDR, temp, THIGH_REG);
+
+    return status;
+}
+
+int read_tlow_reg(uint16_t *temp)
+{
+	uint16_t data = 0;
+
+	int status = i2c_read_bytes(TEMPERATURE_SENSOR_ADDR, (uint8_t *)&data, TLOW_REG, sizeof(status));
+	if (status == -1)
+		return status;
+
+	*temp = data;
+
+	return status;
+}
+
+int read_thigh_reg(uint16_t *temp)
+{
+	uint16_t data = 0;
+
+	int status = i2c_read_bytes(TEMPERATURE_SENSOR_ADDR, (uint8_t *)&data, THIGH_REG, sizeof(status));
+	if (status == -1)
+		return status;
+
+	*temp = data;
+	return status;
+}
+
+int get_sensortemp(uint16_t *temp)
+{
+	uint8_t buff[2] = {0};
+	unsigned char MSB, LSB;
+	int data;
+
+	int status = i2c_read_bytes(TEMPERATURE_SENSOR_ADDR, buff, TEMPERATURE_REG, sizeof(buff));
+	if (status == -1)
+		return status;
+
+	data = ((MSB << 8) | LSB) >> 4;
+	*temp = data*0.0625;
+
+	return status;
+}
